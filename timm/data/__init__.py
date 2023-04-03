04313197600d40065b8007513caae758dfafc48d
Kevin Tse <ktse@fb.com>
2023-04-03 15:26:39 -0400

    Further extending TorchData example

--- timm/data/__init__.py
+++ timm/data/__init__.py
@@ -2,11 +2,13 @@ from .auto_augment import RandAugment, AutoAugment, rand_augment_ops, auto_augme
     rand_augment_transform, auto_augment_transform
 from .config import resolve_data_config, resolve_model_data_config
 from .constants import *
+from .datapipe import create_datapipe
 from .dataset import ImageDataset, IterableImageDataset, AugMixDataset
 from .dataset_factory import create_dataset
 from .dataset_info import DatasetInfo, CustomDatasetInfo
 from .imagenet_info import ImageNetInfo, infer_imagenet_subset
 from .loader import create_loader
+from .loader2 import create_loader2
 from .mixup import Mixup, FastCollateMixup
 from .readers import create_reader
 from .readers import get_img_extensions, is_img_extension, set_img_extensions, add_img_extensions, del_img_extensions
--- timm/data/datapipe.py
+++ timm/data/datapipe.py
@@ -5,6 +5,9 @@ from typing import Dict, List, Optional, Union
 
 
 def image_decode(file_handle, image_format: str = 'RGB'):
+    """
+    Decode image and convert to specific `image_format`.
+    """
     with BytesIO(file_handle.read()) as b:
         img = Image.open(b)
         img.load()
@@ -14,6 +17,9 @@ def image_decode(file_handle, image_format: str = 'RGB'):
 
 
 def decode(item):
+    """
+    Decode the item based on file type. In this case, we have `.cls` and `.jpg` files.
+    """
     key, value = item
     if key.endswith(".cls"):
         return key, int(value.read().decode("utf-8"))
@@ -22,12 +28,18 @@ def decode(item):
 
 
 def select(web_dataset_dict: Dict, img_key: str = '.jpg', target_key: str = '.cls'):
+    """
+    Given a `Dict` object, return a tuple of `(image, target_label)`.
+    """
     return web_dataset_dict[img_key], web_dataset_dict[target_key]
 
 
 def create_files_datapipe(paths: List[str], file_system: str = "local"):
     """
     Given a file system and paths, returns an IterDataPipe of file handlers.
+
+    `.sharding_filter()` will shard the workload across nodes and workers within each node.
+    In this case, each worker will read a different set of tar archives.
     """
     paths_dp = IterableWrapper(paths)
     if file_system == "local":
@@ -51,14 +63,24 @@ def create_datapipe(
         root: Union[str, List[str]],
         split: str,
         file_system: str = "local",
-        download: bool = False,
-        batch_size: Optional[int] = None,
-        seed: int = 42,
+        # download: bool = False,  # Not used for now
+        # batch_size: Optional[int] = None,  # Batch size will be defined later while creating `DataLoader2`
+        # seed: int = 42,  # Seed will be set later by `DataLoader2`
         repeats: int = 0,
         **kwargs
 ) -> IterDataPipe:
     r"""
+    Given a `root` and `file_system`, create a datapipe line that finds the corresponding files and decode them.
 
+    Directory set up as follows:
+        ./train
+            imagenet-train-00001.tar
+            imagenet-train-00002.tar
+            imagenet-train-00003.tar
+        ./valid
+            imagenet-val-00001.tar
+            imagenet-val-00002.tar
+            imagenet-val-00003.tar
     """
     if isinstance(root, str):
         root = [root]
@@ -66,10 +88,18 @@ def create_datapipe(
     if file_system == "local":
         root = [path + f"/{split}/"for path in root]
 
-    files_datapipe = create_files_datapipe(root, file_system)
-    datapipe = files_datapipe.load_from_tar() \
+    tar_archives_dp = create_files_datapipe(root, file_system)
+
+    # Note: after `.map(decode)`, we have an iterable of decoded files that were stored inside the tar archives.
+    #   e.g. `00001000.cls`, `00001000.jpg`, `00001001.cls`, `00001001.jpg`, ...
+    # The built-in `.webdataset()` DataPipe groups the corresponding files into a `Dict`,
+    # such that you will have one `Dict` for each sample (i.e. one for `00001000`, one for `00001001`).
+    datapipe = tar_archives_dp.load_from_tar() \
                    .map(decode) \
                    .webdataset() \
                    .map(select)
+
+    if repeats > 0:
+        datapipe.cycle(repeats)
 
     return datapipe
